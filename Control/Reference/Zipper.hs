681907d2a95802a6c1af8b4fb150d8d517ab78ea
nboldi <nboldi@elte.hu>
2015-05-18 19:07:05 +0200

    generalized zipper

--- Control/Reference/Zipper.hs
+++ Control/Reference/Zipper.hs
@@ -9,43 +9,54 @@ import Control.Reference.Operators
 import Control.Reference.Combinators
 import Control.Reference.TupleInstances
 
-data Zipper h s t a b = Zipper { zipperFocus :: h s t a b
-                               , zipperWhole :: s
-                               }
+data Zipper h (w :: * -> *) (r :: * -> *) s t a b 
+  = Zipper { zipperFocus :: h w r s t a b
+           , zipperWhole :: s
+           }
 
-data (:>) h x y s t a b = h s t x y :> Reference Identity Identity MU MU s t a b
+data (:>) h x y w r s t a b = h w r s t x y :> Reference w r MU MU s t a b
 
-data Top a b c d where
-  Top :: Top a b a b
+data Top (w :: * -> *) (r :: * -> *) a b c d where
+  Top :: Top w r a b a b
 
-type SndZip = (:>) Top (Int,String) (Int,String) (Int,String) (Int,String) String String
-sz :: SndZip
+type SndZip w r = (:>) Top (Int,String) (Int,String) w r (Int,String) (Int,String) String String
+sz :: SndZip Identity Identity
 sz = Top :> _2
 
-type SndSndZip = (:>) ((:>) Top (Int,(Int,String)) (Int,(Int,String))) (Int,String) (Int,String) (Int,(Int,String)) (Int,(Int,String)) String String
+type SndSndZip w r = (:>) ((:>) Top (Int,(Int,String)) (Int,(Int,String))) (Int,String) (Int,String) w r (Int,(Int,String)) (Int,(Int,String)) String String
 
-ssz :: SndSndZip
+ssz :: SndSndZip Identity Identity
 ssz = Top :> _2 :> (_2&_2)
 
-test = move _2 $ move _2 $ zipper (2::Int, (1::Int, "almafa")) 
+test = let zippo = move _2 $ move _2 $ zipper (2::Int, (1::Int, "almafa")) 
+        in zipRef zippo .= "b" $ zipperWhole zippo
 
 class HistoryLens h where
-  historyLens :: h s t a b -> Reference Identity Identity MU MU s t a b
+  historyLens :: RefMonads w r => h w r s t a b -> Reference w r MU MU s t a b
 instance HistoryLens Top where
   historyLens Top = self
 instance HistoryLens ((:>) h x y) where
   historyLens (_ :> r) = r
 
 
-zipper :: a -> Zipper Top a b a b
+zipper :: RefMonads w r => a -> Zipper Top r w a b a b
 zipper = Zipper Top
 
-close :: Zipper Top a b a b -> a
+close :: Zipper Top r w a b a b -> a
 close (Zipper Top a) = a
 
-move :: HistoryLens h => Lens a b c d -> Zipper h s t a b -> Zipper ((:>) h a b) s t c d
+zipRef :: (RefMonads w r, HistoryLens h) => Zipper h w r s t a b -> Reference w r MU MU s t a b
+zipRef z = historyLens (zipperFocus z)
+
+setFocus :: b -> Zipper h w r s t a b -> Zipper h w r t t b b 
+setFocus v z = z { zipperWhole = zipRef z .= v $ zipperWhole z }
+
+move :: (RefMonads w r, HistoryLens h) 
+     => Reference w r MU MU a b c d 
+     -> Zipper h w r s t a b 
+     -> Zipper ((:>) h a b) w r s t c d
 move l (Zipper h s) = Zipper (h :> (historyLens h & l)) s
 
-moveUp :: Zipper ((:>) h a b) s t c d -> Zipper h s t a b
+moveUp :: Zipper ((:>) h a b) w r s t c d -> Zipper h w r s t a b
 moveUp (Zipper (h :> _) s) = Zipper h s
 
